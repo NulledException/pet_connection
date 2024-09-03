f772814bade6298e9b0e034e7b535c784142a2e0
Eduardo Chiminazzo Cagol <51205460+NulledException@users.noreply.github.com>
2024-09-02 22:17:15 -0300

    update: remove unused imports

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,4 @@
 import { Sequelize } from "sequelize"; // importar o sequelize
-import dotenv from "dotenv/config.js"; // importar o dotenv para localizar as variáveis de ambiente
 
 const dbName = process.env.DB_NAME; // passar os dados do .env para as constantes
 const dbUser = process.env.DB_USER;
